cd5990a1eab3a7b56e8a7d4f727ce7793659f014
Itsjustrick12 <Rickmjenkins900@gmail.com>
2023-09-23 13:33:32 -0400

    Player move forward
    i want to update

--- Assets/Scripts/Platformer/PlayerMovement.cs
+++ Assets/Scripts/Platformer/PlayerMovement.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private int moveSpeed = 3;
+    [SerializeField] private float moveX;
+
+    [SerializeField] Rigidbody2D rb;
+
+    
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float moveX = Input.GetAxisRaw("Horizantal") * moveSpeed;
+        if (moveX > 0)
+        {
+            rb.AddForce(new Vector2(moveX, 0f));
+        }
     }
 }
