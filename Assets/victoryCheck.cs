c1354ecb01a2921a055bf9e41fc14530d0c8e86b
Itsjustrick12 <Rickmjenkins900@gmail.com>
2023-09-24 08:56:02 -0400

    Level 1 Finalized

--- Assets/victoryCheck.cs
+++ Assets/victoryCheck.cs
@@ -9,10 +9,11 @@ public class victoryCheck : MonoBehaviour
     [SerializeField] public bool levelCleared = false;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player" && FindObjectOfType<PlayerMovement>().hasPhone)
         {
             //Check that the player correfctly beat the level
             timerText.color = Color.green;
+            
             levelCleared = true;
         }
     }
